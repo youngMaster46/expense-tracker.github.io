917660260a5b8cc255fb516029876e647f7ddf6e
Maxim Kaplin <kaplin95@inbox.ru>
2020-06-09 09:40:39 +0300

    add update values functions

--- script.js
+++ script.js
@@ -6,11 +6,26 @@ const form = document.getElementById('form')
 const text = document.getElementById('text')
 const amount = document.getElementById('amount')
 
-const dummyTransactions = [
-    {id: 1, text: 'Flower', amount:-20},
-    {id: 2, text: 'Salary', amount:300},
-    {id: 3, text: 'Book', amount:-10},
-    {id: 4, text: 'Camera', amount:150},
+const dummyTransactions = [{
+        id: 1,
+        text: 'Flower',
+        amount: -20
+    },
+    {
+        id: 2,
+        text: 'Salary',
+        amount: 300
+    },
+    {
+        id: 3,
+        text: 'Book',
+        amount: -10
+    },
+    {
+        id: 4,
+        text: 'Camera',
+        amount: 150
+    },
 ];
 let transactions = dummyTransactions;
 
@@ -30,10 +45,32 @@ function addTransactionDOM(transaction) {
     list.appendChild(item)
 }
 
+// Update the balance, income and expense
+function updateValues() {
+    const amounts = transactions.map(transaction => transaction.amount)
+
+    const total = amounts.reduce((acc, item) => acc += item, 0).toFixed(2)
+
+    const income = amounts
+        .filter(item => item > 0)
+        .reduce((acc, item) => acc += item, 0)
+        .toFixed(2)
+    const expense = amounts
+        .filter(item => item < 0)
+        .reduce((acc, item) => acc += item  * -1, 0)
+        .toFixed(2)
+
+balance.innerText = `$${total}`       
+moneyPlus.innerText = `$${income}`
+moneyMinus.innerText = `$${expense}`
+}
+
 // Init app
 function init() {
     list.innerHTML = '';
 
     transactions.forEach(addTransactionDOM);
+    updateValues()
 }
+
 init();
